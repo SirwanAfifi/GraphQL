a6f3bc442e16819e649b788d67572970efd8fa95
Sirwan Afifi <sir1afifi@gmail.com>
2018-10-07 01:46:38 +0330

    initial graphql setup

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import express from "express";
+import  graphqlHTTP from "express-graphql";
+import schema from "./schema";
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("GraphQ: is amazing!");
 });
+
+const root = { hello: () => "Hi, I'm Sirwan"};
+
+app.use("/graphql", graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
 
 app.listen(3000, () => { console.log("Running server on port localhost:3000/graphq;")});
