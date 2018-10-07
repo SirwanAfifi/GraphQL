7161aeab19e025c973449b68b115d8f0ba83ac0a
Sirwan Afifi <sir1afifi@gmail.com>
2018-10-07 13:35:21 +0330

    changed email object type

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 import express from "express";
-import  graphqlHTTP from "express-graphql";
+import graphqlHTTP from "express-graphql";
 import schema from "./schema";
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("GraphQ: is amazing!");
+  res.send("GraphQ: is amazing!");
 });
 
-const root = { friend: () => {
+const root = {
+  friend: () => {
     return {
-        "id": 232324323,
-        "firstName": "Sirwan",
-        "lastName": "Afifi",
-        "gender": "male",
-        "language": "Kurdish",
-        "email": "sir1afifi@gmail.com"
-    }
-}};
+      id: 232324323,
+      firstName: "Sirwan",
+      lastName: "Afifi",
+      gender: "male",
+      language: "Kurdish",
+      emails: [
+        { email: "sir1afifi@gmail.com" },
+        { email: "email2@gmail.com" },
+        { email: "email3@gmail.com" }
+      ]
+    };
+  }
+};
 
-app.use("/graphql", graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema: schema,
     rootValue: root,
     graphiql: true
-}));
+  })
+);
 
-app.listen(3000, () => { console.log("Running server on port localhost:3000/graphq;")});
+app.listen(3000, () => {
+  console.log("Running server on port localhost:3000/graphq;");
+});
